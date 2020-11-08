75b68db717b82aff3b22e570a0f48a9bd40936cf
swaraj747 <swaraj.borikar95@gmail.com>
2020-11-08 11:45:59 +0000

    This is swaraj package modification

--- swaraj.py
+++ swaraj.py
@@ -1,4 +1,5 @@
 print("This is a swaraj borikar test file")
 print("This is a first change made by Mr. Swaraj Borikar")
 print("This is called confidence")
+print("swaraj is a person with 10 LAC Package")
 
